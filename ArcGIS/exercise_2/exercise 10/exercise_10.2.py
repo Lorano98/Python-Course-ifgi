4e62a5c5192ba0f72a12e58cb22624ad5c6965f3
Laxeros07 <annestaskie@gmail.com>
2024-06-20 13:03:49 +0200

    10.2

---
+++ ArcGIS/exercise_2/exercise 10/exercise_10.2.py
@@ -0,0 +1,32 @@
+# Modify this file to be used with arcpy.GetParametersAsText(0) and arcpy.AddMessage()
+
+import arcpy, sys
+
+arcpy.env.overwriteOutput = True
+# parameters
+in_fc = arcpy.GetParameterAsText(0)
+
+# checking that parameters are correct
+print(f"Input Path {in_fc}")
+
+# run work
+# near - save the distances in the attribute table
+near = arcpy.analysis.Near(in_fc,"stops_ms_mitte",distance_unit="Meters")
+
+# create the search cursor
+scur = arcpy.da.SearchCursor(in_fc, ["NEAR_FID","NEAR_DIST"])
+near_id = 0
+dist = 0
+# save the id of and the distance to the next busstop
+for row in scur:
+    near_id = row[0]
+    dist = round(row[1],2)
+# build sql
+sql = f"OBJECTID={near_id}"
+# Search for the next busstop to save the name
+scur = arcpy.da.SearchCursor(in_table="stops_ms_mitte",field_names=['name'],where_clause=sql)
+for row in scur:
+    arcpy.AddMessage(f"Name Haltestelle: {row[0]}")    
+arcpy.AddMessage(f"Entfernung: {dist}m")
+
+print('Work Done')
